2a8060c20492a394a4448160acfae1a161dc035e
MyNameIsYGKim <kelen536@gmail.com>
2024-12-04 17:46:50 +0900

    ss

--- src/components/homeComponent.jsx
+++ src/components/homeComponent.jsx
@@ -25,35 +25,49 @@ export const Div = styled.div`
   &.phone-headerbar {
     display: flex;
     justify-content: space-between;
+    align-items: center;
     padding: 10px;
   }
 
   .phone-menuBtn {
+    align-items: center;
+    background-color: white;
+    border: none;
+    border-radius: 100%;
+    box-sizing: border-box;
     display: flex;
     justify-content: center;
-    align-items: center;
-    border: none;
-    background-color: transparent;
+    padding: 5px;
     cursor: pointer;
     svg {
-      width: 30px;
-      height: 30px;
+      width: 20px;
+      height: 20px;
+    }
+    path {
+      fill: black;
+    }
+    &:hover {
+      scale: 1.2;
     }
   }
 
   &.phone-headerRight {
     display: flex;
     gap: 10px;
+    position: absolute;
+    right: 10px;
     .phone-themeBtn {
       display: flex;
       justify-content: center;
       align-items: center;
       border: none;
-      background-color: transparent;
+      border-radius: 100%;
+      background-color: white;
+      padding: 2.5px 4px 2.5px 1px;
       cursor: pointer;
       svg {
-        width: 30px;
-        height: 30px;
+        width: 25px;
+        height: 25px;
         path:nth-child(1) {
           fill: rgb(220,110,50);
         }
@@ -61,33 +75,21 @@ export const Div = styled.div`
           fill: yellow;
         }
       }
+      &:hover {
+        scale: 1.2;
+      }
     }
     .phone-searchBox {
       display: flex;
       justify-content: center;
       align-items: center;
-      background-color: transparent;
-      border: 1px solid black;
+      background-color: white;
+      /* border: 1px solid black; */
       padding: 0 3px;
       border-radius: 15px;
-      .closeBtn {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border-radius: 50%;
-        /* color: white; */
-        font-size: 1rem;
-        padding-left: 5px;
-        border: none;
-        background-color: transparent;
-        cursor: pointer;
-        svg {
-          display: flex;
-          width: 21px;
-          height: 21px;
-        }
-      }
+      box-sizing: border-box;
       input {
+        width: 100px;
         height: 100%;
         border: none;
         outline: none;
@@ -97,7 +99,6 @@ export const Div = styled.div`
         margin-left: 10px;
       }
       .searchBtn {
-        border-radius: 50%;
         width: 30px;
         height: 30px;
         display: flex;
@@ -109,9 +110,30 @@ export const Div = styled.div`
         svg {
           width: 70%;
           height: 70%;
-          circle, path {
-            /* color: white; */
-          }
+        }
+        &:hover {
+          scale: 1.2;
+        }
+      }
+      .closeBtn {
+        width: 30px;
+        height: 30px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        /* color: white; */
+        font-size: 1rem;
+        padding-left: 5px;
+        border: none;
+        background-color: transparent;
+        cursor: pointer;
+        svg {
+          display: flex;
+          width: 21px;
+          height: 21px;
+        }
+        &:hover {
+          scale: 1.2;
         }
       }
     }
@@ -120,27 +142,36 @@ export const Div = styled.div`
       justify-content: center;
       align-items: center;
       border: none;
-      background-color: transparent;
+      border-radius: 50%;
+      background-color: white;
+      padding: 2.5px;
       cursor: pointer;
       svg {
-        width: 30px;
-        height: 30px;
+        width: 25px;
+        height: 25px;
+      }
+      &:hover {
+        scale: 1.2;
       }
     }
   }
 
-  &.phone-header-theme {
+  &.phone-theme {
     display: flex;
     height: 100%;
     justify-content: center;
     align-items: center;
   }
 
-  &.phone-header-theme button {
+  &.phone-theme button {
     font-size: 3rem;
     border: none;
     background-color: transparent;
     padding: 0;
+    cursor: pointer;
+    &:hover {
+      color: #555;
+    }
   }
 
   &.diary-container {
@@ -151,24 +182,32 @@ export const Div = styled.div`
 
   &.diary-box {
     background-color: #fff;
-    margin: 0 10px;
     border-radius: 10px;
     margin: 10px;
+    padding: 10px;
+    &:hover {
+      padding: 5px;
+      border: 5px solid skyblue;
+    }
     .diary-date {
       font-size: 24px;
       font-weight: 700;
       margin: 0;
-      padding: 10px;
+      border-radius: 10px;
     }
     .diary-title {
-      font-size: 16px;
+      font-size: 20px;
       font-weight: 500;
       margin: 0;
-      padding: 0 10px;
     }
     .diary-desc {
       margin: 0;
-      padding: 10px;
+      display: -webkit-box;
+      -webkit-line-clamp: 1; /* 최대 3줄로 제한 */
+      -webkit-box-orient: vertical;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      max-height: calc(1.5em * 3); /* 3줄까지만 높이 제한 */
     }
   }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useContext, useState } from "react";
+import { useEffect, useContext, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   Container,
@@ -11,10 +11,10 @@ import theme1 from "../assets/images/theme1.jpg";
 import { UserContext } from "../contexts/UserContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCalendar, faPerson } from "@fortawesome/free-solid-svg-icons";
-import { RxHamburgerMenu } from "react-icons/rx";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { AiOutlineClose } from "react-icons/ai";
 import { LuSearch, LuPaintbrush } from "react-icons/lu";
-import { BsSortDownAlt } from "react-icons/bs";
+import { BsSortNumericDown, BsSortNumericDownAlt } from "react-icons/bs";
 import AxiosApi from "../api/AxiosApi";
 
 const monthName = new Date().toLocaleString("default", { month: "long" });
@@ -24,7 +24,7 @@ const Home = () => {
   const { userId } = useContext(UserContext);
   const [diaries, setDiaries] = useState([]);
   const navigate = useNavigate();
-
+  
   useEffect(() => {
     const getDiaries = async (userId) => {
       try {
@@ -38,17 +38,48 @@ const Home = () => {
     getDiaries(userId);
   }, [userId]);
 
+  
+
+  // 검색창 열고 닫기
   const [searchVisible, setSearchVisible] = useState(false);
   const [searchCloseVisible, setSearchCloseVisible] = useState(false);
-
   const searchVisibleOn = () => {
     setSearchVisible(true);
     setSearchCloseVisible(true);
   }
+  useEffect(()=> {  // 커서 포커싱
+    if (searchVisible) {
+      inputRef.current.focus();
+    }
+  }, [searchVisible]);
   const searchVisibleOff = () => {
     setSearchVisible(false);
     setSearchCloseVisible(false);
+    setSearchValue("");
   }
+
+  // 검색
+  const inputRef = useRef(null);
+  const [searchValue, setSearchValue] = useState("");
+  const inputChange = (e) => {
+    setSearchValue(e.target.value);
+  }
+
+  // 정렬
+  const [isSort, setIsSort] = useState("asc");
+  const handleSort = () => {
+    setIsSort((prevState) => (prevState === "asc" ? "desc" : "asc"));
+  }
+
+  // 날짜 선택
+  const selectDate = () => {
+    
+  }
+
+  // 일기 날짜 포맷
+  const currentDate = new Date();
+  const [diaryYear, setDiaryYear] = useState(currentDate.getFullYear());
+  const [diaryMonth, getDiaryMonth] = useState(currentDate.getMonth() + 1);
 
   return (
     <Container>
@@ -56,20 +87,31 @@ const Home = () => {
         {/* 헤더바 */}
         <Div className="phone-header">
           <Img1 src={theme1} alt="theme1" />
-
           <Div className="phone-headerbar">
             <Div className="phone-headerLeft">
-              {/* 왼쪽 메뉴 버튼 */}
               <button className="phone-menuBtn">
-                <RxHamburgerMenu />
+                <GiHamburgerMenu />
               </button>
             </Div>
-
             <Div className="phone-headerRight">
               <button className="phone-themeBtn">
                 <LuPaintbrush />
               </button>
               <div className="phone-searchBox"> 
+                <input 
+                  type="text" 
+                  style={{ display: searchVisible ? "block" : "none"}} 
+                  value={searchValue} 
+                  onChange={inputChange} 
+                  ref={inputRef} 
+                />
+                <button 
+                  className="searchBtn" 
+                  onClick={searchVisibleOn} 
+                  style={{ display: searchVisible ? "none" : "block"}}
+                >
+                  <LuSearch />
+                </button>
                 <button 
                   className="closeBtn" 
                   onClick={searchVisibleOff}
@@ -77,23 +119,16 @@ const Home = () => {
                 >
                   <AiOutlineClose />
                 </button>
-                <input style={{ display: searchVisible ? "block" : "none"}} />
-                <button 
-                  className="searchBtn" 
-                  onClick={searchVisibleOn}
-                >
-                  <LuSearch />
-                </button>
               </div>
-              <button className="phone-sort">
-                <BsSortDownAlt />
+              <button className="phone-sort" onClick={handleSort}>
+                {isSort === "asc" ? <BsSortNumericDown /> : <BsSortNumericDownAlt />}
               </button>
             </Div>
           </Div>
 
-          <Div className="phone-header-theme">
-            <button>
-              {year} {monthName}
+          <Div className="phone-theme">
+            <button onClick={selectDate}>
+              {diaryYear} {diaryMonth}월
             </button>
           </Div>
         </Div>
@@ -102,7 +137,29 @@ const Home = () => {
           {diaries.length === 0 ? (
             <p>추가된 일기가 아직 없습니다.</p>
           ) : (
-            diaries.map((diary, index) => (
+            diaries
+            .slice() // 원본 배열을 복사해 정렬
+            .sort((a,b) => {
+              const dayA = new Date(a.publishedAt).getDate();
+              const dayB = new Date(b.publishedAt).getDate();
+              
+              if(isSort === "asc") {
+                return dayA - dayB;
+              } else if(isSort === "desc") {
+                return dayB - dayA;
+              }
+              return 0;
+            })
+            .filter(diary => {
+              const matchDate = 
+                new Date(diary.publishedAt).getFullYear() === diaryYear 
+                && new Date(diary.publishedAt).getMonth()+1 === diaryMonth;
+              const matchSearch = 
+                diary.title.toLowerCase().includes(searchValue.toLowerCase()) 
+                || diary.content.toLowerCase().includes(searchValue.toLowerCase())
+              return matchDate && matchSearch;
+            })
+            .map((diary, index) => (
               <Div
                 key={index}
                 className="diary-box"
@@ -111,9 +168,15 @@ const Home = () => {
                   () => navigate("/diaryInsert", { state: { diary, index } }) // 일기 데이터랑 해당 인덱스 보내기...
                 }
               >
-                <p className="diary-date">{diary.date}</p>
+                {/* {console.log(diaries)} */}
+                <p className="diary-date">
+                {(() => {
+                  const date = new Date(diary.publishedAt);
+                  return date.getDate().toString().padStart(2, '0');
+                })()}
+                </p>
                 <p className="diary-title">{diary.title}</p>
-                <p className="diary-desc">{diary.description}</p>
+                <p className="diary-desc">{diary.content}</p>
               </Div>
             ))
           )}
