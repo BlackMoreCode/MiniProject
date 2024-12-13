50290cf12bf039fc5a2668f8a7138e69edb8e5a1
donghyun1013 <aq2132@naver.com>
2024-12-13 10:53:48 +0900

    폰트 더블 클릭 시 변경

--- src/pages/MyPage/ChangeFont.js
+++ src/pages/MyPage/ChangeFont.js
@@ -4,7 +4,6 @@ import { LoginContext } from "../../contexts/LoginContext";
 import "../../styles/Font/fonts.css";
 import styled from "styled-components";
 import { Container, Div } from "./MyPageStyles";
-import leftArrowIcon from "../../assets/icons/left-arrow.png";
 import { IoIosArrowBack } from "react-icons/io";
 
 const ChangeFont = () => {
@@ -13,90 +12,63 @@ const ChangeFont = () => {
   const [currentFont, setCurrentFont] = useState(selectedFont); // 로컬 상태 추가
   const { isDarkMode } = useContext(LoginContext); // Dark 모드
 
-  const fontOptions = [
-    { label: "Do Hyeon", class: "font-dohyeon" },
-    { label: "Gowun Dodum", class: "font-gowundodum" },
-    { label: "Hi Melody", class: "font-himelody" },
-    { label: "Jua", class: "font-jua" },
-    { label: "Nanum Gothic", class: "font-nanumgothic" },
+  const fontClass = [
+    "font-basic",
+    "font-dohyeon",
+    "font-gowun-dodum",
+    "font-hi-melody",
+    "font-jua",
+    "font-nanum-gothic",
+  ];
+  const fontTitle = [
+    "Basic",
+    "Do Hyeon",
+    "Gowun Dodum",
+    "Hi Melody",
+    "Jua",
+    "Nanum Gothic",
   ];
 
-  const handleFontChange = (fontClass) => {
+  const fontData = fontClass.map((fontClass, index) => ({
+    class: fontClass,
+    title: fontTitle[index],
+  }));
+
+  const handleFontDoubleClick = (fontClass) => {
     setSelectedFont(fontClass); // 폰트 상태 업데이트
     setCurrentFont(fontClass); // 로컬 상태 업데이트
     document.body.className = fontClass; // DOM 업데이트
+    alert(`${fontClass} 폰트가 적용되었습니다.`);
+    navigate("/");
   };
-
-  // 폰트박스 요소 배열
-  const fontClass = [
-    'font-basic',
-    'font-dohyun',
-    'font-gowun-dodum',
-    'font-hi-melody',
-    'font-jua',
-    'font-nanum-gothic',
-  ];
-  const fontTitle = [
-    'Basic',
-    'Do Hyeon',
-    'Gowun Dodum',
-    'Hi Melody',
-    'Jua',
-    'Nanum Gothic',
-  ];
-
-  const fontData = [
-    { class: fontClass[0], title: fontTitle[0] },
-    { class: fontClass[1], title: fontTitle[1] },
-    { class: fontClass[2], title: fontTitle[2] },
-    { class: fontClass[3], title: fontTitle[3] },
-    { class: fontClass[4], title: fontTitle[4] },
-    { class: fontClass[5], title: fontTitle[5] },
-    { class: fontClass[5], title: fontTitle[5] },
-    { class: fontClass[5], title: fontTitle[5] },
-    { class: fontClass[5], title: fontTitle[5] },
-    { class: fontClass[5], title: fontTitle[5] },
-    { class: fontClass[5], title: fontTitle[5] },
-  ];
 
   return (
     <Container>
       <Div className={isDarkMode ? "font-container-dark" : "font-container"}>
         <div className="font-header">
-          <button onClick={()=>navigate("/mypage")} className="backBtn">
+          <button onClick={() => navigate("/mypage")} className="backBtn">
             <IoIosArrowBack />
           </button>
-          <p onClick={()=>navigate("/mypage")} className="mypage-title">폰트</p>
+          <p onClick={() => navigate("/mypage")} className="mypage-title">
+            폰트
+          </p>
         </div>
 
         <div className="font-body">
-          {/* {fontOptions.map((font) => (
-            <button
-              key={font.class}
-              onClick={() => handleFontChange(font.class)}
-              className={`${font.class} ${
-                currentFont === font.class ? "selected" : ""
-              }`}
-            >
-              {font.label}
-            </button>
-          ))} */}
-
           {fontData.map((font, index) => (
-            <div key={index} className={`font-box ${font.class}`}>
+            <div
+              key={index}
+              className={`font-box ${font.class}`}
+              onDoubleClick={() => handleFontDoubleClick(font.class)} // 더블 클릭 이벤트 추가
+            >
               <p>&lt;&lt; {font.title} &gt;&gt;</p>
               <p>내일은 내일의 태양이 뜬다.</p>
               <p>Manners, Maketh, Man.</p>
             </div>
           ))}
         </div>
-
-        
-        {/* <button onClick={() => navigate("/")}>폰트 변경</button>{" "} */}
-        {/* 버튼 텍스트 변경 */}
       </Div>
     </Container>
-    
   );
 };
 
