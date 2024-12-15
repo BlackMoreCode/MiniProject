d5fdbfc3b2c5481156b3fac006c7a8e8f3999c48
Lee Sangwon <83111797+swon2id@users.noreply.github.com>
2024-12-15 17:26:00 +0900

    design: 인트로 페이지 UI 레이아웃 변경

--- src/pages/Intro/Intro.jsx
+++ src/pages/Intro/Intro.jsx
@@ -1,68 +1,34 @@
 import { useEffect, useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { IntroContents } from "./IntroStyles";
-import logoIcon from "../../assets/icons/logo.png";
-import introPageWomanImage from "../../assets/images/intro-page-woman.png";
-import introPageManImage from "../../assets/images/intro-page-man.png";
+import logoIcon from "../../assets/icons/logo.svg";
 import { LoginContext } from "../../contexts/LoginContext";
 
-
 const Intro = () => {
-  const context = useContext(LoginContext);
-  const { userId, userPassword } = context;
-  const navigate = useNavigate();
+  const { loggedInMember } = useContext(LoginContext);
+  const navigator = useNavigate();
 
   useEffect(() => {
-    console.log(userId, userPassword)
-    if (userId && userPassword) {
-      navigate("/");
-    }
-  }, [userId, userPassword])
+    if (loggedInMember) navigator("/home");
+  }, [loggedInMember]);
 
   return (
     <IntroContents>
-      <header>
-        <img src={logoIcon} alt="CODELOG 로고" />
-      </header>
-      <main>
-        <section>
-          <div>
-            <img src={introPageWomanImage} alt="" />
-            <div>
-              <p>"이 코드... 뭐였더라?"</p>
-            </div>
-          </div>
-          <div>
-            <p>코드로그에서 오늘의 코딩일기를</p>
-            <p>기록해보세요!</p>
-          </div>
-        </section>
-        <section>
-          <div>
-            <div>
-              <p>"프로젝트 일정이</p>
-              <p>어떻게 됐었지?"</p>
-            </div>
-            <img src={introPageManImage} alt="" />
-          </div>
-          <div>
-            <p>코드로그에서 프로젝트 일정을</p>
-            <p>관리해보세요!</p>
-          </div>
-        </section>
-        <section>
-          <div>
-            <p>성장하는 개발자를 위한</p>
-            <p>나만의 다이어리, 코드로그</p>
-          </div>
-        </section>
-        <nav>
-          <div>
-            <Link to="/login">로그인</Link>
-            <Link to="/signup">회원가입</Link>
-          </div>
-        </nav>
-      </main>
+      <img src={logoIcon} alt="CODELOG 로고" />
+      <button
+        onClick={() => {
+          navigator("/login");
+        }}
+      >
+        로그인
+      </button>
+      <button
+        onClick={() => {
+          navigator("/signup");
+        }}
+      >
+        가입하기
+      </button>
     </IntroContents>
   );
 };
